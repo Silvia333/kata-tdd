646d33161e78f1f9dd3b873b0fb347318da31027
silviadelucas <sdelucas33@gmail.com>
2018-05-06 17:51:56 +0200

    upd: comprobar objeto

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -2,6 +2,8 @@
 *
 *   velocidad de respuesta, acierto o fallo,
 *
+*       Si no hay preguntas.
+*
 *       Si acierto pregunta en menos de 2 segundos - sumo 2 puntos
 *           (0 puntos, correcta, 1 segundo) -> 2 puntos
 *           (2 puntos, correcta, 0 segundo) -> 4 puntos 
@@ -25,60 +27,161 @@
 *           (0, fallo, 9 segundos) -> -1 puntos
 *
 * */
+describe('comprobador de respuestas', function() {
+    function comprobarRespuesta(pregunta, respuesta){
+        return pregunta.correcta===parseInt(respuesta);
+    }
+    var preguntaPrueba = {
+        pregunta : '¿Capital de Honduras?',
+        respuestas : [
+            'Tegucigalpa',
+            'Tokio',
+            'Madrid'
+        ],
+        correcta: 0
+    };
+   
+  it('comprobar si ha acertado', function(){
+    expect(comprobarRespuesta(preguntaPrueba, 0)).toBeTruthy();
+  }); 
+});
+
+describe('comprobador de respuestas', function() {
+        function comprobarObjeto(pregunta){
+            return typeof pregunta.correcta === 'number';
+        }
+        var preguntaPrueba2 = {
+            pregunta : '¿Capital de Honduras?',
+            respuestas : [
+                'Tegucigalpa',
+                'Tokio',
+                'Madrid'
+            ],
+            correcta: null
+        };
+  it('comprobar el formato del objeto pregunta', function(){
+    expect(comprobarObjeto(preguntaPrueba2)).toBeFalsy();
+  }); 
+});
+
+describe('comprobador de respuestas', function() {
+	function isCorrect(question, userSelectedAnswer) {
+		if(question.id !== userSelectedAnswer.questionId) {
+			return false;
+		}
+		if (userSelectedAnswer.id !== question.correctAnswer.id) {
+			return false;
+		}
+		return true;
+    }
+    
+    it("reconoce una pregunta correcta", function() {
+		expect(isCorrect({
+						id: 1,
+						question: '¿Cual es la capital de Portugal?',
+						answers: [
+							{id: 1, value: 'Faro'},
+							{id: 2, value: 'Oporto'},
+							{id: 3, value: 'Lisboa'},
+
+						],
+						correctAnswer: {id: 3}
+					},
+					{questionId: 1, id: 3 })
+			).toBeTruthy();
+		});
+	it("reconoce una pregunta incorrecta", function() {
+		expect(isCorrect({
+						id: 1,
+						question: '¿Cual es la capital de Portugal?',
+						answers: [
+							{id: 1, value: 'Faro'},
+							{id: 2, value: 'Oporto'},
+							{id: 3, value: 'Lisboa'},
+
+						],
+						correctAnswer: {id: 3}
+					},
+					{questionId: 1, id: 2 })
+		).toBeFalsy();
+	});
+	it("reconoce una respuesta que no corresponde a la pregunta", function() {
+		expect(isCorrect({
+					id: 1,
+					question: '¿Cual es la capital de Portugal?',
+					answers: [
+						{id: 1, value: 'Faro'},
+						{id: 2, value: 'Oporto'},
+						{id: 3, value: 'Lisboa'},
+
+					],
+					correctAnswer: {id: 3}
+				},
+				{questionId: 5, id: 3 })
+		).toBeFalsy();
+	});
+});
 
 
 
 describe('calculo de marcador', function(){
-    function recalcularMarcador(puntos, esCorrecta, tiempo){
-        if (esCorrecta && tiempo <= 2){
-            return puntos + 2;
-        }
-        if (esCorrecta && tiempo >= 2 && tiempo < 10){
-            return ++puntos;
-        }
-        if (esCorrecta && tiempo > 10){
-            return puntos;
-        }
-        //No es coherente esta comprobación con la siguiente.
-        // if ( tiempo === ""){
-        //     return 'Tienes que responder';
-        // }
-
-        if (esCorrecta === '' && tiempo > 20){
-            return puntos - 3;
-        }
-        if (!esCorrecta && tiempo > 10){
+    // funcion true
+    // funcion false
+    function recalcularNoContesta(puntos, tiempo){
+        if (tiempo === ''){
             return puntos - 2;
         }
-        if (!esCorrecta && tiempo < 10){
+        if (tiempo > 20){
+            return puntos - 3;
+        }
+    }
+
+    function recalcularMarcadorAcierto(puntos, tiempo){
+        if (tiempo <= 2){
+            return puntos + 2;
+        }
+        if (tiempo >= 2 && tiempo < 10){
+            return ++puntos;
+        }
+        if (tiempo > 10){
+            return puntos;
+        }
+    }
+
+    function recalcularMarcadorFallo(puntos, tiempo){
+        if (tiempo > 10){
+            return puntos - 2;
+        }
+        if (tiempo <= 10){
             return puntos - 1;
         }
     }
+
     it("suma mas puntos si acierta muy rapido en menos de 2 segundos", function(){
-        expect(recalcularMarcador(0, true, 1)).toBe(2);
+        expect(recalcularMarcadorAcierto(0, 1)).toBe(2);
     });
 
     it("suma 1 punto si acierta entre 2 y 10 segundos", function(){
-        expect(recalcularMarcador(3, true, 3)).toBe(4);
+        expect(recalcularMarcadorAcierto(3, 3)).toBe(4);
     });
 
     it("suma 0 puntos si acierta en más de 10 segundos", function(){
-        expect(recalcularMarcador(10, true, 15)).toBe(10);
+        expect(recalcularMarcadorAcierto(10, 15)).toBe(10);
     });
 
     // it("Si no se responde no se puede seguir", function(){
-    //     expect(recalcularMarcador(10, '', '')).toBe('Tienes que responder');
+    //     expect(recalcularNoContesta(0)).toBe(-2);
     // });
 
     it("Si en 20 segundos no has respondido, pasa a siguiente pregunta", function(){
-        expect(recalcularMarcador(3, '', 21)).toBe(0);
-    });
+        expect(recalcularNoContesta(3, 21)).toBe(0);
+    }); 
 
     it("Si fallas en mas de 10 segundos pierdes puntos", function(){
-        expect(recalcularMarcador(3, false, 21)).toBe(1);
+        expect(recalcularMarcadorFallo(3, 21)).toBe(1);
     });
 
     it("Si fallas en menos de 10 segundos pierdes puntos", function(){
-        expect(recalcularMarcador(3, false, 9)).toBe(2);
+        expect(recalcularMarcadorFallo(0, 9)).toBe(-1);
     });
 });
